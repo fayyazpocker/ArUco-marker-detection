3100e15c72fb0f541428d159e0c393f48062e10f
fayyazpocker <fayyazpocker@gmail.com>
2018-04-04 16:19:06 +0530

    Update 2.aruco_detection.py

--- 2.aruco_detection.py
+++ 2.aruco_detection.py
@@ -8,22 +8,14 @@ cap = cv2.VideoCapture(0)
 
 
 det_aruco_list = {}
-last_time=0
 
 while (True):
 	ret,frame = cap.read()
-	#print frame.shape
 	det_aruco_list = detect_Aruco(frame)
-	cur_time = int(round(time.time() * 1000))
 	if(det_aruco_list):
-
 		img = mark_Aruco(frame,det_aruco_list)
 		robot_state = calculate_Robot_State(img,det_aruco_list)
-		#print robot_state
-		#print min(robot_state.keys()), robot_state[(min(robot_state.keys()))]
-		#cv2.circle(img,(255,50),1,(0,0,255),2)
-		#print cur_time - last_time
-		last_time = cur_time
+		
 	cv2.imshow('image',frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
